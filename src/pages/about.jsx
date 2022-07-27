a2dc74d407683a4be9f36e7683b30ede7a482fe0
Paige Vogenthaler <paige.vogenthaler@gmail.com>
2022-07-26 20:48:54 -0500

    add linkedin embed

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,75 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import { Helmet } from "react-helmet";
 
-const About = (props) => (
-  <Layout
-    title="About"
-    className="about"
-    subtitle={
-      <p>
-        Find me all over the web <b>@paigevogie</b>.
-      </p>
-    }
-    {...props}
-  >
-    <Helmet>
-      <script
-        async
-        src="https://platform.twitter.com/widgets.js"
-        charset="utf-8"
-      ></script>
-      <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
-    </Helmet>
-    <div className="embeds-wrapper">
-      <div className="embeds">
-        <div className="twitter-embed">
-          <a
-            class="twitter-timeline"
-            target="_blank"
-            href="https://twitter.com/paigevogie?ref_src=twsrc%5Etfw"
-          >
-            Tweets by paigevogie
-          </a>
-        </div>
-        <iframe
-          title="Spotify Playlist"
-          className="spotify-embed"
-          src="https://open.spotify.com/embed/playlist/3eZXX8QCpHVxWgUypND5Lb?utm_source=generator"
-          frameBorder="0"
-          allowFullScreen=""
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        ></iframe>
-        <iframe
-          title="Strava Runs"
-          className="strava-embed"
-          src="https://www.strava.com/athletes/55412609/latest-rides/66f1db3bfecd2a5d3c8fb9e689c8e825d99673df"
-          frameBorder="0"
-          scrolling="yes"
-        ></iframe>
-        <div className="pinterest-embed-sm">
-          <a
-            data-pin-do="embedUser"
-            data-pin-board-width="180"
-            data-pin-scale-height="90"
-            data-pin-scale-width="70"
-            target="_blank"
-            href="https://www.pinterest.com/paigevogie/"
-          ></a>
-        </div>
-        <div className="pinterest-embed-lg">
-          <a
-            data-pin-do="embedUser"
-            data-pin-board-width="325"
-            data-pin-scale-height="240"
-            data-pin-scale-width="60"
-            target="_blank"
-            href="https://www.pinterest.com/paigevogie/"
-          ></a>
+const About = (props) => {
+  const [showLinkedIn, setShowLinkedIn] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener(
+      "error",
+      (event) => {
+        /* If linkedin errors, safari is preventing cross-site tracking */
+        if (!!event.target && event.target.src.includes("linkedin")) {
+          setShowLinkedIn(false);
+        }
+      },
+      { capture: true }
+    );
+  }, []);
+
+  return (
+    <Layout
+      title="About"
+      className="about"
+      subtitle={
+        <p>
+          Find me all over the web <b>@paigevogie</b>.
+        </p>
+      }
+      {...props}
+    >
+      <Helmet>
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        ></script>
+        <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
+        <script
+          src="https://platform.linkedin.com/badges/js/profile.js"
+          async
+          defer
+          type="text/javascript"
+        ></script>
+      </Helmet>
+      <div className="embeds-wrapper">
+        <div className="embeds">
+          {/* Twitter */}
+          <div className="twitter-embed">
+            <a
+              className="twitter-timeline"
+              target="_blank noreferrer"
+              href="https://twitter.com/paigevogie?ref_src=twsrc%5Etfw"
+            >
+              Paige Vogie on Twitter.
+            </a>
+          </div>
+          {/* Spotify */}
+          <iframe
+            title="Spotify Playlist"
+            className="spotify-embed"
+            src="https://open.spotify.com/embed/playlist/3eZXX8QCpHVxWgUypND5Lb?utm_source=generator"
+            frameBorder="0"
+            allowFullScreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          ></iframe>
+          {/* Strava */}
+          <iframe
+            title="Strava Runs"
+            className="strava-embed"
+            src="https://www.strava.com/athletes/55412609/latest-rides/66f1db3bfecd2a5d3c8fb9e689c8e825d99673df"
+            frameBorder="0"
+            scrolling="yes"
+          ></iframe>
+          {/* Pinterest */}
+          <div className="pinterest-embed-sm">
+            <a
+              data-pin-do="embedUser"
+              data-pin-board-width="180"
+              data-pin-scale-height="90"
+              data-pin-scale-width="70"
+              target="_blank noreferrer"
+              href="https://www.pinterest.com/paigevogie/"
+            >
+              Paige Vogie on Pinterest.
+            </a>
+          </div>
+          <div className="pinterest-embed-lg">
+            <a
+              data-pin-do="embedUser"
+              data-pin-board-width="325"
+              data-pin-scale-height="240"
+              data-pin-scale-width="60"
+              target="_blank noreferrer"
+              href="https://www.pinterest.com/paigevogie/"
+            >
+              Paige Vogie on Pinterest.
+            </a>
+          </div>
+          {/* LinkedIn */}
+          {showLinkedIn && (
+            <div
+              className="linkedin-embed badge-base LI-profile-badge"
+              data-locale="en_US"
+              data-size="large"
+              data-theme="light"
+              data-type="HORIZONTAL"
+              data-vanity="paigevogie"
+              data-version="v1"
+            >
+              <a
+                className="badge-base__link LI-simple-link"
+                href="https://www.linkedin.com/in/paigevogie"
+              >
+                Paige Vogie on LinkedIn.
+              </a>
+            </div>
+          )}
         </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default About;
