c8e356204666a252a709b21da0d0cb4cb2057949
Paige Vogenthaler <paige.vogenthaler@gmail.com>
2025-04-18 00:21:26 -0500

    add link to storygraph book

--- pages/components/StoryGraph/index.jsx
+++ pages/components/StoryGraph/index.jsx
@@ -4,14 +4,16 @@ const StoryGraph = ({ storygraphData }) => (
   <div className={styles["storygraph-embed"]}>
     <div>Recently Read</div>
     <ul>
-      {storygraphData.books.map(({ title, author, img }) => (
+      {storygraphData.books.map(({ title, author, img, url }) => (
         <li key={`${title}${author}`}>
-          <img src={img} alt={title} />
-          <div>
-            <div>{title}</div>
-            <small>{author}</small>
-          </div>
-      </li>
+          <a href={url} target="_blank">
+            <img src={img} alt={title} />
+            <div>
+              <div>{title}</div>
+              <small>{author}</small>
+            </div>
+          </a>
+        </li>
       ))}
     </ul>
   </div>
